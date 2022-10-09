10efded144108369ebe4fd2489810083f781596e
fishruff <arsershoff@gmail.com>
2022-10-09 19:39:08 +0300

    1

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const menu = document.querySelector('.burgerMenu');
 const button = document.querySelector('.burgerBtn');
-const navItem = document.querySelectorAll('.navItem');
-const navigationItem = document.querySelector('.navigation').querySelectorAll('a');
+// const navItem = document.querySelectorAll('.navItem');
+// const navigationItem = document.querySelector('.navigation').querySelectorAll('a');
 const desc = document.querySelector('.descComplecs');
 const complecsItem = document.querySelector('.complecs').querySelectorAll('a');
 
 console.log(navItem);
 
-navItem.forEach(element => {
-    element.addEventListener('mouseover', ()=>{
-        if(element.classList.value == 'navItem hoverLink')
-        element.classList.remove('hoverLink')
-        else
-        element.classList.add('hoverLink')
-    } )
-});
+// navItem.forEach(element => {
+//     element.addEventListener('mouseover', ()=>{
+//         if(element.classList.value == 'navItem hoverLink')
+//         element.classList.remove('hoverLink')
+//         else
+//         element.classList.add('hoverLink')
+//     } )
+// });
 
 complecsItem.forEach(element => {
     element.addEventListener('mouseover', ()=>{
